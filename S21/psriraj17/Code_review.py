2d6792ab2bb63771cc23ae7302e54ae930fc452d
psriraj17 <78946867+psriraj17@users.noreply.github.com>
2021-03-12 10:49:17 -0500

    Update Code_review.py

--- S21/psriraj17/Code_review.py
+++ S21/psriraj17/Code_review.py
@@ -1,4 +1,4 @@
-
+# funtion to return turkish word
 def turkish(str):
     lower_map = {
         ord(u'I'): u'ı',
@@ -6,7 +6,7 @@ def turkish(str):
         }
     lower_str = str.translate(lower_map)
     print(lower_str.lower())
-
+#funtion to return irish word
 def irish(str):
     vowels= ['A','E','I','O','U','Á','É','Í','Ó','Ú']
     for i in range(len(vowels)):
@@ -15,7 +15,7 @@ def irish(str):
             print(str.lower())
     else:
         print(str.lower())
-
+#funtion to return greek word
 def Greek(word):
     #ΠΣhΣgfΣdsΌΛΗΣΣΣΣ
         leng = len(word) - 1
@@ -31,13 +31,13 @@ def Greek(word):
 
 if __name__ == '__main__':
 
- lang = input("Enter your language (insert either symbol or language name):  ")
- str  = input("Enter your String:  " )
+ lang = input("Enter your language (insert either symbol or language name):  ") #User input of language
+ str  = input("Enter your String:  " ) # userinput for word
  if lang== "Turkish" or lang== "turkish" or lang== "tr":
-    turkish(str)
+    turkish(str) #function call 
  if lang== "Irish" or lang== "ga" or lang== "irish" or lang== "ga-IE":
-     irish(str)
+     irish(str)# function call
  if lang== "Greek" or lang== "el" or lang== "greek":
-     Greek(str)
+     Greek(str)# function call
 else:
      print(str.lower())
